6a63cb8e22be2335e5ba1a453140f7d31b3adafa
123-prog <2571374157@qq.com>
2025-11-17 17:59:06 +0800

    format py

--- decompiler.py
+++ decompiler.py
@@ -8,6 +8,7 @@ java_decompiler_path = "src/java-decompiler.jar"
 jar_path = "ezjson.jar"
 output_path = "result/"
 
+
 def check_path():
     p = Path(java_decompiler_path)
     if p.is_file() == False:
@@ -26,13 +27,15 @@ def check_path():
 
     return True
 
+
 def getJarName(jar_path):
     return jar_path.split("/")[-1]
 
 
-#从jar反编译出java文件
+# 从jar反编译出java文件
 def decompiler_jar():
-    cmd_txt = "java -cp {} org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -dgs=true {} {}".format(java_decompiler_path,jar_path,output_path)
+    cmd_txt = "java -cp {} org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -dgs=true {} {}".format(
+        java_decompiler_path, jar_path, output_path)
     _subProcess = subprocess.getstatusoutput(cmd_txt)
 
     if _subProcess[0] != 0:
@@ -41,27 +44,31 @@ def decompiler_jar():
         print(_subProcess[1])
         print("INFO:\tDecompiler class to java file successfully")
 
-    with zipfile.ZipFile(Path(output_path).joinpath(getJarName(jar_path)),mode='r') as zfile:
+    with zipfile.ZipFile(Path(output_path).joinpath(getJarName(jar_path)), mode='r') as zfile:
         for file in zfile.namelist():
-            zfile.extract(file,Path(output_path).joinpath(getJarName(jar_path).rstrip('.jar')+"_tmp"))
+            zfile.extract(file, Path(output_path).joinpath(
+                getJarName(jar_path).rstrip('.jar') + "_tmp"))
 
         Path(output_path).joinpath(getJarName(jar_path)).unlink()
+
 
 # 根据反编译生成的文件，调整源码位置和依赖配置，生成一份项目源代码（可打包）
 # maven项目
 def gen():
-    #copy demo to des
+    # copy demo to des
     demo = "src/maven_demo"
     res = Path("result/").joinpath(getJarName(jar_path).rstrip(".jar"))
-    shutil.copytree(demo,res)
+    shutil.copytree(demo, res)
 
-    src_path = Path(output_path).joinpath(getJarName(jar_path).rstrip('.jar')+"_tmp")
+    src_path = Path(output_path).joinpath(
+        getJarName(jar_path).rstrip('.jar') + "_tmp")
     src = src_path.joinpath("BOOT-INF/classes/com")
     des = res.joinpath("src/main/java/com")
-    shutil.copytree(src,des)
+    shutil.copytree(src, des)
 
-    shutil.move(src_path.joinpath("BOOT-INF/classes/application.properties"),res.joinpath("src/main/resources/application.properties"))
-    
+    shutil.move(src_path.joinpath("BOOT-INF/classes/application.properties"),
+                res.joinpath("src/main/resources/application.properties"))
+
     meta_inf_path = src_path.joinpath("META-INF/maven/")
     pom_path = ""
 
@@ -70,18 +77,20 @@ def gen():
             pom_path = i
 
     if pom_path != "":
-        shutil.move(pom_path,res.joinpath("pom.xml"))
+        shutil.move(pom_path, res.joinpath("pom.xml"))
         shutil.rmtree(src_path)
     else:
         print("pom.xml is not exists")
 
+
 if __name__ == '__main__':
-    
+
     epilog = r'''Example:
     python3 decompiler.py -jar ./ezjson.jar -o ./result/
     '''
 
-    parse = argparse.ArgumentParser(epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parse = argparse.ArgumentParser(
+        epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
     parse.add_argument('-jar', '--jar', help='jar包路径')
     parse.add_argument('-o', '--out', help='输入存放反编译后文件的目录')
 
@@ -90,7 +99,7 @@ if __name__ == '__main__':
     jar_path = args.jar
     output_path = args.out
 
-    if jar_path is not None and output_path is not None:        
+    if jar_path is not None and output_path is not None:
         decompiler_jar()
         gen()
     else:
